a96ac71abdf0c7029b4588e552f51acc17608268
nguy4130 <nguy4130@umn.edu>
2020-09-05 23:03:24 -0500

    completed A*

--- src/PathFindingCore.java
+++ src/PathFindingCore.java
@@ -10,9 +10,14 @@ public class PathFindingCore {
   public static void main(String[] args){
     int[] start = {3, 0};
     int[] end = {3, 3};
-    List<NodeDijkstra> path = dijkstra(map1, start, end);
-    for(int i = path.size() -1; i >= 0; i--){
-      System.out.println(path.get(i).toString());
+    List<NodeDijkstra> pathDijkstra = dijkstra(map1, start, end);
+    for(int i = pathDijkstra.size() -1; i >= 0; i--){
+      System.out.println(pathDijkstra.get(i).toString());
+    }
+    List<NodeAStar> pathAStar = astar(map1, start, end);
+    System.out.println();
+    for(int i = pathAStar.size() -1; i >= 0; i--){
+      System.out.println(pathAStar.get(i).toString());
     }
   }
   /** Dijkstra's Search */
@@ -95,13 +100,89 @@ public class PathFindingCore {
   }
 
   /** A* Search */
-  List<NodeAStar> pathAStar = new ArrayList<>();
+  public static List<NodeAStar> astar(int[][] map, int[] start, int[] end){
+    //Create start and end node
+    NodeAStar startNode = new NodeAStar(null, start);
+    NodeAStar endNode = new NodeAStar(null, end);
+
+    //Initialize open and closed lists
+    List<NodeAStar> openList = new ArrayList<>();
+    List<NodeAStar> closedList = new ArrayList<>();
+
+    //Add startNode to openList
+    openList.add(startNode);
+
+    //Loop to find the path
+    while(!openList.isEmpty()){
+      //Get current node
+      NodeAStar currentNode = openList.get(0);
+      int currentIndex = 0;
+      for(int i = 0; i < openList.size(); i++){
+        if(openList.get(i).f < currentNode.f){
+          currentNode = openList.get(i);
+          currentIndex = i;
+        }
+      }
+      
+      //Pop current off open list, add to closed list
+      openList.remove(currentIndex);
+      closedList.add(currentNode);
+      
+      //Found the goal
+      if(currentNode.match(endNode)){
+        List<NodeAStar> pathAStar = new ArrayList<>();
+        NodeAStar current = currentNode;
+        while(current != null){
+          pathAStar.add(current);
+          current = current.parent;
+        }
+        return pathAStar;
+      }
+
+      //Generate children
+      List<NodeAStar> children = new ArrayList<>();
+      int[] dx = {0, 0, -1, 1};
+      int[] dy = {-1, 1, 0, 0};
+      for(int move = 0; move < 4; move++){
+        int[] newPosition = new int[2];
+        newPosition[0] = currentNode.position[0] + dx[move];
+        newPosition[1] = currentNode.position[1] + dy[move];
+
+        //Check within range and walkable terrain
+        if(newPosition[0] <= (map.length - 1) && 
+            newPosition[0] >= 0 &&
+            newPosition[1] <= (map[map.length - 1].length - 1) && 
+            newPosition[1] >= 0 && 
+            map[newPosition[0]][newPosition[1]] == 0){
+              NodeAStar newChildNode = new NodeAStar(currentNode, newPosition);
+              children.add(newChildNode);
+            }
+      }
+
+      //Loop through children nodes
+      for(NodeAStar child: children){
+        if(!closedList.contains(child)){
+          child.g = currentNode.g + 1;
+          child.h = (child.position[0] - endNode.position[0]) * (child.position[0] - endNode.position[0]) +
+                    (child.position[1] - endNode.position[1]) * (child.position[1] - endNode.position[1]);
+          child.f = child.g + child.h;
+        }
+        if(!openList.contains(child)){
+          openList.add(child);
+        }
+      }
+    }
+    return new ArrayList<>();
+  }
+  /** ARA* Search */
+  /*double e = 2.5;
+  List<NodeARA> pathARA = new ArrayList<>();
   public boolean validNode(int[] position, int size){
     return (position[0]>=0) && (position[0]<size) && (position[1]>=0) && (position[1]<size);
   }
 
-  public double minInconAndOpen(List<NodeAStar> inconList, List<NodeAStar> openList){
-    NodeAStar minNode = null;
+  public double minInconAndOpen(List<NodeARA> inconList, List<NodeARA> openList){
+    NodeARA minNode = null;
     if(!inconList.isEmpty()){
       minNode = inconList.get(0);
       for(int i = 0; i < inconList.size(); i++){
@@ -121,13 +202,96 @@ public class PathFindingCore {
     return minNode.g + minNode.h;
   }
 
-  public void backTrack(NodeAStar current, NodeAStar start, NodeAStar goal, int[][] a){
+  public void backTrack(NodeARA current, NodeARA start, NodeARA goal, int[][] a){
     if(current.parent != null){
       backTrack(current.parent, start, goal, a);
     }
     if(current.notMatch(goal) && current.notMatch(start) && (a != null)){
       a[current.position[0]][current.position[1]] = 2;
     }
-
   }
+
+  public void finalBackTrack(NodeARA current, NodeARA start, NodeARA goal, int [][] a){
+    if(current.parent != null){
+      finalBackTrack(current.parent, start, goal, a);
+    }
+    if(current.notMatch(goal) && current.notMatch(start) && (a != null)){
+      a[current.position[0]][current.position[1]] = 2;
+    }
+    pathARA.add(current);
+  }
+
+  public boolean inArray(NodeARA[] a, int[] position){
+    for(int i = 0; i < a.length; i++){
+      if((a[i].position[0] == position[0]) && (a[i].position[1] == position[1])){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void addToInconList(List<NodeARA> inconList, NodeARA current){
+    if(!inconList.contains(current)){
+      inconList.add(current);
+    }
+    else{
+      for(int i = 0; i < inconList.size(); i++){
+        if(inconList.get(i).isMatch(current)){
+          inconList.set(i, current);
+        }
+      }
+    }
+  }
+
+  public void improvePath(
+    int[][] a, 
+    NodeARA start, 
+    NodeARA goal, 
+    int size, 
+    Queue<NodeARA> openList, 
+    List<NodeARA> closedList, 
+    List<NodeARA> inconList,
+    NodeARA[][] parentMatrix){
+      int[] dx = {0, 0, -1, 1};
+      int[] dy = {-1, 1, 0, 0};
+      NodeARA parent = start;
+      parentMatrix[parent.position[0]][parent.position[1]] = parent;
+      int count = 2;
+      while(!openList.isEmpty()){
+        if(goal.fvalue(e) > openList.peek().fvalue(e)){
+          parent = openList.poll();
+          closedList.add(parent);
+          if(parent.notMatch(goal)){
+            for(int i = 0; i < 4; i++){
+              int[] pos = new int(2);
+              pos[0] = parent.position[0] + dx[i]; //new x
+              int x = pos[0];
+              pos[1] = parent.position[1] + dy[i]; //new y
+              int y = pos[1];
+              if(validNode(pos, size)){
+                if(a[x][y] != 1){
+                  if(parentMatrix[x][y] != null){
+                    NodeARA temp = new NodeARA(pos, parent, goal);
+                    temp.g = Integer.MAX_VALUE;
+                    parentMatrix[x][y] = temp;
+                  }
+                  if(parentMatrix[x][y].g > (1 + parent.g)){
+                    parentMatrix[x][y].update(parent, goal);
+                    NodeARA temp = parentMatrix[x][y];
+                    if(!inArray(closedList, pos)){
+                      if(openList.contains(parentMatrix[x][y])){
+                        int openListSize = openList.size();
+                        for(int j = 0; j < openListSize; j++){
+                          open
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }*/
 }
